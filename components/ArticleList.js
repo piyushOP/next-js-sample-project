e9cc883db731f3d0cae394f6ffc9168e0af8a6d8
piyush-011 <piyush@quickwork.co>
2021-11-09 10:31:55 +0530

    first commit

---
+++ components/ArticleList.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-key */
+import ArticleItem from "./ArticleItem";
+import articleStyles from "../styles/Article.module.css";
+
+const ArticleList = ({ articles }) => {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => (
+        <ArticleItem key={article.id} article={article} />
+      ))}
+    </div>
+  );
+};
+
+export default ArticleList;
